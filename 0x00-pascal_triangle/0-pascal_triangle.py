304dcf527ba2d96b7fbd001b3628013c46d7b8b3
afuawonders <afuawonders@gmail.com>
2024-02-06 00:23:06 +0000

    P Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle of size n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]  # First element of every row is 1
+
+        if triangle:
+            last_row = triangle[-1]
+            # Generate elements from the second to second-to-last
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)  # Last element of every row is 1
+
+        triangle.append(row)
+
+    return triangle
+
+if __name__ == "__main__":
+    pascal_triangle_result = pascal_triangle(5)
+
+    for row in pascal_triangle_result:
+        print("[{}]".format(",".join([str(x) for x in row])))
