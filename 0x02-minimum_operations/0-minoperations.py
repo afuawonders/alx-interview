88635dfba2c064fb1aabc1169c9b379165d4b491
afuawonders <afuawonders@gmail.com>
2024-02-20 23:13:35 +0000

    min_op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,35 +1,60 @@
 #!/usr/bin/python3
-'''Calculates the fewest number of operations needed to get exactly n H characters'''
+'''Minimum Operations python3 challenge'''
+
 
 def minOperations(n):
-    '''Returns 0 if n is impossible to achieve'''
-    pasted_chars = 1  # chars in the file
-    clipboard = 0  # H's copied
+    '''calculates the fewest number of
+    operations needed to result in exactly n H
+    characters in this file.
+    Returns:
+        Integer : if n is impossible to achieve, return 0
+    '''
+    pasted_chars = 1  # how many chars in the file
+    clipboard = 0  # how many H's copied
     counter = 0  # operations counter
 
     while pasted_chars < n:
-        if clipboard == 0:  # if not copied yet
-            clipboard = pasted_chars  # copy all
-            counter += 1  # increment counter
+        # if did not copy anything yet
+        if clipboard == 0:
+            # copyall
+            clipboard = pasted_chars
+            # increment operations counter
+            counter += 1
 
-        if pasted_chars == 1:  # if not pasted yet
-            pasted_chars += clipboard  # paste
-            counter += 1  # increment counter
-            continue  # continue loop
+        # if haven't pasted anything yet
+        if pasted_chars == 1:
+            # paste
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 1
+            # continue to next loop
+            continue
 
-        remaining = n - pasted_chars  # remaining chars to paste
-        if remaining < clipboard:  # check if impossible
-            return 0  # return 0
+        remaining = n - pasted_chars  # remaining chars to Paste
+        # check if impossible by checking if clipboard
+        # has more than needed to reach the number desired
+        # which also means num of chars in file is equal
+        # or more than in the clipboard.
+        # in both situations it's impossible to achieve n of chars
+        if remaining < clipboard:
+            return 0
 
-        if remaining % pasted_chars != 0:  # if can't be divided
-            pasted_chars += clipboard  # paste current clipboard
-            counter += 1  # increment counter
+        # if can't be devided
+        if remaining % pasted_chars != 0:
+            # paste current clipboard
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 1
         else:
-            clipboard = pasted_chars  # copy all
-            pasted_chars += clipboard  # paste
-            counter += 2  # increment counter
+            # copyall
+            clipboard = pasted_chars
+            # paste
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 2
 
-    if pasted_chars == n:  # if got desired result
+    # if got the desired result
+    if pasted_chars == n:
         return counter
     else:
         return 0
