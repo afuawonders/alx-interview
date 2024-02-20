fbac2c98b3565d53da78bdcde90079fdaf616de5
afuawonders <afuawonders@gmail.com>
2024-02-20 23:09:09 +0000

    min_op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-'''
-Given a number n, write a method that
-calculates the fewest number of operations
-needed to result in exactly n H characters
-in the file.
-'''
-
+'''Calculates the fewest number of operations needed to get exactly n H characters'''
 
 def minOperations(n):
-    '''
-    Prototype: def minOperations(n)
-    Returns an integer
-    If n is impossible to achieve, return
-    '''
-    if n <= 1:
+    '''Returns 0 if n is impossible to achieve'''
+    pasted_chars = 1  # chars in the file
+    clipboard = 0  # H's copied
+    counter = 0  # operations counter
+
+    while pasted_chars < n:
+        if clipboard == 0:  # if not copied yet
+            clipboard = pasted_chars  # copy all
+            counter += 1  # increment counter
+
+        if pasted_chars == 1:  # if not pasted yet
+            pasted_chars += clipboard  # paste
+            counter += 1  # increment counter
+            continue  # continue loop
+
+        remaining = n - pasted_chars  # remaining chars to paste
+        if remaining < clipboard:  # check if impossible
+            return 0  # return 0
+
+        if remaining % pasted_chars != 0:  # if can't be divided
+            pasted_chars += clipboard  # paste current clipboard
+            counter += 1  # increment counter
+        else:
+            clipboard = pasted_chars  # copy all
+            pasted_chars += clipboard  # paste
+            counter += 2  # increment counter
+
+    if pasted_chars == n:  # if got desired result
+        return counter
+    else:
         return 0
-
-    operations = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        operations[i] = i
-        for j in range(2, i // 2 + 1):
-            if i % j == 0:
-                operations[i] = min(operations[i], operations[j] + i // j)
-    return operations[n]
